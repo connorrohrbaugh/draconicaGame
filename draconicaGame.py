3b43feb7c47e7d6f98242cd974d207f489669616
Taylor Cohron <taylor.cohron@me.com>
2014-04-04 09:35:34 -0400

    Beginning of the game file
    Main python class for game, more may be added.

---
+++ draconicaGame.py
@@ -0,0 +1,15 @@
+#####################
+##    DRACONICA    ##
+##     TAYLOR      ##
+##     4/4/14      ##
+#####################
+
+#IMPORT STUFF
+from graphics import *
+import random
+import time
+import math
+
+def main():
+  #PUT STUFF HERE
+main()
